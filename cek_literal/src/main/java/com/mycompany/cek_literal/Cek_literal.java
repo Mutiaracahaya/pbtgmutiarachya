6900d5c7e3e507e379325c7be673d45210c73322
Mutiaracahaya <145527452+Mutiaracahaya@users.noreply.github.com>
2024-09-11 08:12:17 +0700

    Add files via upload

---
+++ cek_literal/src/main/java/com/mycompany/cek_literal/Cek_literal.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cek_literal;
+
+/**
+ *
+ * @author Thinkpad-T450
+ */
+public class Cek_literal {
+
+    public static void main(String[] args) {
+        // Data Variabel dan jenis tipe datanya
+        long c = 4L;  // Menggunakan L untuk mendefinisikan long
+        int i = 4;
+        double a = 4d;
+        boolean b = true;
+        String nama = "Ahmad Dhani";
+        char x = 'X';
+
+        // Menampilkan nilai variabel
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(nama);
+        System.out.println(x);
+    }
+}
---
+++ halodunia/src/main/java/com/mycompany/halodunia/Halodunia.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.halodunia;
+
+/**
+ *
+ * @author Thinkpad-T450
+ */
+public class Halodunia {
+
+    public static void main(String[] args) {
+        // Nama file: HalloDunia.java
+// Program sederhana untuk mencetak informasi pengguna dengan karakter khusus
+
+        // Mencetak informasi pengguna dengan karakter khusus
+        System.out.println("Nama : Tuliskan Nama Anda\b");    // \b untuk backspace
+        System.out.println("Gender : Tuliskan Gender Anda\n"); // \n untuk baris baru
+        System.out.println("Alamat :\tTuliskan Alamat Anda");  // \t untuk tab
+        System.out.println("Tanggal Lahir : Tuliskan Tanggal Lahir Anda\r"); // \r untuk carriage return
+        System.out.println("\\*Karakter Khusus\\*");  // \\ untuk mencetak backslash dan \* untuk bintang
+        System.out.println("Nama Lengkap: \'Andika\'");  // \' untuk mencetak tanda petik tunggal
+        System.out.println("Path: C:\\Users\\NamaAnda\\");  // \\ untuk mencetak tanda backslash
+    }
+}
+
+    
+
